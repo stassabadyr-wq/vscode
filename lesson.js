23fd0dd24c91f8e26191ec5d850cacf60f7162ba
stassabadyr-wq <stassabadyr@gmail.com>
2026-02-11 15:28:40 +0300

    Проверка номера телефона

--- lesson.js
+++ lesson.js
@@ -1,4 +1,11 @@
-const normalizeUrl = (url) => url.startsWith('https://') ? url : `https://${url}`
+const normalizePhone = (number = '') => {
+  if (!number) return '';
+  
+  if (number.startsWith('+7')) return number;
+  if (number.startsWith('8')) return '+7' + number.slice(1);
+  if (number.startsWith('7')) return '+' + number;
+  
+  return number;
+};
 
-console.log(normalizeUrl('yandex'))
-console.log(normalizeUrl('https://yandex'))
+console.log(normalizePhone('79788200000'))
